bc2e3d76a43fb6255509eb21052e764120f3a54b
Ashraf Manna <optimisticashraf@gmail.com>
2025-04-15 11:01:11 +0600

    courses part updated

--- src/Hooks/useAxiosPublic.jsx
+++ src/Hooks/useAxiosPublic.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosPublicly = axios.create({
-    baseURL: "https://edu-vibe-back-end.vercel.app"
-    // baseURL: "http://localhost:5000"
+    // baseURL: "https://edu-vibe-back-end.vercel.app"
+    baseURL: "http://localhost:5000"
 })
 
 const useAxiosPublic = () => {
