726a417a3bd4556c44f5c6c56600d568f35f8284
Ashraf Manna <optimisticashraf@gmail.com>
2025-04-16 10:41:02 +0600

    some CSS fixed of About Us Page

--- src/shared components/FaqQuestion.jsx
+++ src/shared components/FaqQuestion.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const FaqQuestion = ({question, answer}) => {
     return (
-        <div className="collapse collapse-plus bg-base-100 border border-base-300">
-        <input type="radio" name="my-accordion-3" defaultChecked />
-        <div className="collapse-title font-semibold">{question}</div>
-        <div className="collapse-content text-sm">{answer}</div>
+        <div className="collapse collapse-plus bg-gray-300 dark:bg-gray-800 text-gray-700 dark:text-white border-none">
+        <input type="radio" name="my-accordion-3" className='bg-gray-300 dark:bg-gray-800 text-gray-700 dark:text-white' defaultChecked />
+        <div className="collapse-title font-semibold bg-gray-300 dark:bg-gray-800 text-gray-700 dark:text-white">{question}</div>
+        <div className="collapse-content text-sm bg-gray-300 dark:bg-gray-800 text-gray-700 dark:text-white">{answer}</div>
     </div>
     );
 };
