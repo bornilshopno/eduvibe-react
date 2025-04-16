4a1080d732313b9713a69f6870da20caaf8e98f8
Ashraf Manna <optimisticashraf@gmail.com>
2025-04-16 10:45:25 +0600

    backend link updated

--- src/Hooks/useAxiosPublic.jsx
+++ src/Hooks/useAxiosPublic.jsx
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const axiosPublicly = axios.create({
     // baseURL: "https://edu-vibe-back-end.vercel.app"
-    baseURL: "http://localhost:5000"
+    // baseURL: "http://localhost:5000"
+    baseURL: "https://eduvibe-react-backend.onrender.com"
 })
 
 const useAxiosPublic = () => {
